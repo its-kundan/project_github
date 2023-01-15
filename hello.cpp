72ddc8142b758a0f90e36e169981763857c74083
Kundan <kundan51kk@gmail.com>
2023-01-15 14:08:33 +0530

    added cpp file

---
+++ hello.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <functional>
+#include <stack>
+#include <queue>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <map>
+#include <unordered_set>
+#include <math.h>
+using namespace std;
+int32_t main() {
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL) ;
+  cout.tie(NULL);
+ 
+ cout<<"Hello World, welcome";
+  return 0;
+}
